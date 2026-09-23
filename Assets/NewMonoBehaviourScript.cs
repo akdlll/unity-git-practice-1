55c7714292b31c6df71f3e3f8d2d124520c36347
bse0925 <minsoooo34@gmail.com>
2026-09-23 15:27:44 +0900

    f

---
+++ Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    int a;
+    void Start()
+    {
+        a = 123;
+        Debug.Log(a);
+        // Debug.Log("Hellow World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
